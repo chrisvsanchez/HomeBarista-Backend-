8106f9018bd8c702bb782d8dcf4fb8f4df0285a2
chrisvsanchez <chrisvsanchez@gmail.com>
2021-01-27 10:44:07 -0500

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,18 @@ Rails.application.routes.draw do
   resources :posts
   resources :users, only:[:index, :show, :create, :destroy, :update]
   resources :items, only: [:create]
- 
+
   # User Routes   
     post "https://home-barista-api.herokuapp.com/login", to: "users#login"
     get "https://home-barista-api.herokuapp.com/autologin", to: "users#autologin"
     patch "https://home-barista-api.herokuapp.com/ProfilePicture", to: "users#profile_picture"
     post "https://home-barista-api.herokuapp.com/signup", to: "users#create"
   # Feeds
+   
   #Posts
 
 
+  # Reviews 
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
